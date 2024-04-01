743535cf5dbe8414a9f6ba77fd43b8315ecdc7d2
Cesar Lara <celara@imed.cl>
2024-04-01 18:54:08 -0300

    fix layout stablishment

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -161,7 +161,7 @@ export default function Layout(props: LayoutProps) {
                   {/* Sidebar component, swap this element with another sidebar if you like */}
                   <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 ring-1 ring-white/10">
                     <div className="flex h-16 shrink-0 items-center">
-                      <h1 className="h-8 w-auto">{user.establishment.name.toUpperCase()}</h1>
+                      <h1 className="h-8 w-auto">{user.establishment ? user.establishment.name.toUpperCase() : ""}</h1>
                       <img
                         className="h-8 w-auto"
                         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
@@ -215,7 +215,7 @@ export default function Layout(props: LayoutProps) {
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
           <div className="flex h-16 items-center justify-center text-center text-white">
-  <h1 className="h-8 w-auto">{ user.establishment.name.toUpperCase()}</h1>
+  <h1 className="h-8 w-auto">{user.establishment ? user.establishment.name.toUpperCase() : ""}</h1>
 </div>
             <nav className="flex flex-1 flex-col">
               <ul role="list" className="flex flex-1 flex-col gap-y-7">

4577ea4c54728faf880d173d62d5ad948ff33d1f
Cesar Lara <celara@imed.cl>
2024-04-25 20:14:03 -0400

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,23 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Layout from "../components/layout/Layout";
+import { useUserStore } from "../store/userStore";
+import CasesAuthenticated from "../components/CasesAuthenticated";
+import Greetings_Authenticated from "../components/greetings/Authenticated";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { bearer, setRole, GetRole, user, isLoading } = useUserStore();
   return (
-    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-    <div className="px-4 py-3 sm:px-6">
-      <h6 className="font-bold md:text-lg text-sm">
-        En construcción
-      </h6>
+    <>
+      {GetRole() === "Authenticated" && <><Greetings_Authenticated/><CasesAuthenticated /></>}
+      {GetRole() !== "Authenticated" && (
+          <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+        <div className="px-4 py-3 sm:px-6">
+          <h6 className="font-bold md:text-lg text-sm">En construcción</h6>
+        </div>
     </div>
-
-  </div>
-    
+      )}
+    </>
   );
 }

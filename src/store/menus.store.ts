a0e3214d9a7ac0934a801bf2dcacd8d08aef0f27
“Bayron <bayron.saavedra18@gmail.com>
2024-05-03 17:57:59 -0400

    se creo denuncia de profesor

--- src/store/menus.store.ts
+++ src/store/menus.store.ts
@@ -3,80 +3,98 @@ import { devtools, persist, createJSONStorage } from 'zustand/middleware';
 import type { } from '@redux-devtools/extension';
 import { ArchiveBoxIcon, HomeIcon, LightBulbIcon, UsersIcon } from '@heroicons/react/20/solid';
 interface Imenus {
-    name: string;
-    href: string;
-    current:boolean;
-    icon: React.ForwardRefExoticComponent<
+  name: string;
+  href: string;
+  current: boolean;
+  icon: React.ForwardRefExoticComponent<
     Omit<React.SVGProps<SVGSVGElement>, "ref"> & {
-        title?: string;
-        titleId?: string;
+      title?: string;
+      titleId?: string;
     } & React.RefAttributes<SVGSVGElement>
-    >
+  >
 }
 
-const AuthenticatedMenus:Imenus[] = [
-    { name: "Home", href: "/", icon: HomeIcon, current: false },
-    {
-      name: "Te Escuchamos",
-      href: "/te_escuchamos",
-      icon: UsersIcon,
-      current: false,
-    },
-    // { name: "Casos", href: "/casos", icon: UsersIcon, current: false },
-    {
-      name: "Sugerencias",
-      href: "/sugerencia",
-      icon: LightBulbIcon,
-      current: false,
-    },
-  ]
+const AuthenticatedMenus: Imenus[] = [
+  { name: "Home", href: "/", icon: HomeIcon, current: false },
+  {
+    name: "Te Escuchamos",
+    href: "/te_escuchamos",
+    icon: UsersIcon,
+    current: false,
+  },
+  // { name: "Casos", href: "/casos", icon: UsersIcon, current: false },
+  {
+    name: "Sugerencias",
+    href: "/sugerencia",
+    icon: LightBulbIcon,
+    current: false,
+  },
+]
 
-  const EncargadoMenus:Imenus[] = [
-    { name: "Home", href: "/", icon: HomeIcon, current: false },
-    {
-      name: "Denuncia",
-      href: "/casos/crear",
-      icon: UsersIcon,
-      current: false,
-    },
-    // { name: "Casos", href: "/casos", icon: UsersIcon, current: false },
-    {
-      name: "Sugerencias",
-      href: "/casos",
-      icon: LightBulbIcon,
-      current: false,
-    },
-    {
-      name: "Lista de casos",
-      href: "/casos",
-      icon: ArchiveBoxIcon,
-      current: false,
-    },
-  ];
+const EncargadoMenus: Imenus[] = [
+  { name: "Home", href: "/", icon: HomeIcon, current: false },
+  {
+    name: "Denuncia",
+    href: "/casos/crear",
+    icon: UsersIcon,
+    current: false,
+  },
+  // { name: "Casos", href: "/casos", icon: UsersIcon, current: false },
+  {
+    name: "Sugerencias",
+    href: "/casos",
+    icon: LightBulbIcon,
+    current: false,
+  },
+  {
+    name: "Lista de casos",
+    href: "/casos",
+    icon: ArchiveBoxIcon,
+    current: false,
+  },
+];
+
+const ProfesorMenus: Imenus[] = [
+  { name: "Home", href: "/", icon: HomeIcon, current: false },
+  {
+    name: "Denuncia",
+    href: "/casos/denuncia",
+    icon: UsersIcon,
+    current: false,
+  },
+  {
+    name: "Lista de casos",
+    href: "/casos",
+    icon: ArchiveBoxIcon,
+    current: false,
+  },
+];
 
 type Actions = {
   setMenusAuthenticated(): void;
   setMenusEncargado(): void;
-  setActive(href:string): void;
+  setMenusProfesor(): void;
+  setActive(href: string): void;
 };
 type State = {
-    menus:Imenus[];
-  };
-  
-  
-  export const useMenuStore = create<State & Actions>()(
-    devtools(
-        (set, get) => ({
-        menus:[],
-        setMenusAuthenticated: () => set((state) => ({ menus:AuthenticatedMenus  })),
-        setMenusEncargado: () => set((state) => ({ menus:EncargadoMenus  })),
-        setActive: (href: string) =>
-            set((state) => ({
-              menus: state.menus.map((menu) => ({
-                ...menu,
-                current: menu.href === href,
-              })),
-            }))
-        }),
-    {name:'menuStore'})
-  );
+  menus: Imenus[];
+};
+
+
+export const useMenuStore = create<State & Actions>()(
+  devtools(
+    (set, get) => ({
+      menus: [],
+      setMenusAuthenticated: () => set((state) => ({ menus: AuthenticatedMenus })),
+      setMenusEncargado: () => set((state) => ({ menus: EncargadoMenus })),
+      setMenusProfesor: () => set((state) => ({ menus: ProfesorMenus })),
+      setActive: (href: string) =>
+        set((state) => ({
+          menus: state.menus.map((menu) => ({
+            ...menu,
+            current: menu.href === href,
+          })),
+        }))
+    }),
+    { name: 'menuStore' })
+);

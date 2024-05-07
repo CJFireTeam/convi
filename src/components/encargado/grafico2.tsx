e9b93470eb596b3436e34695648d49b884f7dc02
leonardoretamal <leonardo.retamal.morales@gmail.com>
2024-05-07 09:33:47 -0400

    se mejoro grafico

--- src/components/encargado/grafico2.tsx
+++ src/components/encargado/grafico2.tsx
@@ -10,23 +10,18 @@ export const data = {
     datasets: [
         {
             label: '# of Votes',
-            data: [25, 25, 25,25],
+            data: [10, 35, 15,35],
             backgroundColor: [
-              
               'rgba(54, 162, 235, 0.4)', 
               'rgba(139, 69, 19, 0.4)', 
               'rgba(128, 128, 128, 0.4)', 
               'rgba(255, 206, 86, 0.4)',
-      
-             
             ],
             borderColor: [
-              
               'rgba(54, 162, 235, 1)', 
               'rgba(139, 69, 19, 1)', 
               'rgba(128, 128, 128, 1)', 
               'rgba(255, 206, 86, 1)', 
-           
             ],
             borderWidth: 1,
         },

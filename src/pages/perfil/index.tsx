f6b83dad93cc1ecbe3d3df6e9c7734f5d1f36028
Jorge Fuenmayor <jorge.22fv@gmail.com>
2024-05-10 01:57:03 -0400

    Tabla authentiucated
    -Ocultar columnas para el perfil authenticated.
    - Aumentar tamaño de la pagina de perfil

--- src/pages/perfil/index.tsx
+++ src/pages/perfil/index.tsx
@@ -19,28 +19,35 @@ export default function Perfil() {
             <div className="flex flex-col sm:flex-row rounded-lg border shadow-lg animate-fadein p-4 md:p-10 lg:p-10">
 
                 <div className="flex flex-col sm:w-1/2">
-                    <span className="text-base font-semibold leading-6 text-gray-900">Rol:</span>
-                    <span className="mb-2">{role.name}</span>
+                    <h2 className="text-xl font-semibold text-gray-900 mb-6">Perfil de Usuario</h2>
+                    <div className="mb-6">
+                        <span className="text-lg font-semibold text-gray-700">Rol:</span>
+                        <span className="text-base text-gray-900 ml-2">{role.name}</span>
+                    </div>
                     {role.name !== "Authenticated" && (
-                        <>
-                            <span className="text-base font-semibold leading-6 text-gray-900">Establecimiento:</span>
-                            <span className="mb-2">{user.establishment.name}</span>
-                        </>
+                        <div className="mb-6">
+                            <span className="text-lg font-semibold text-gray-700">Establecimiento:</span>
+                            <span className="text-base text-gray-900 ml-2">{user.establishment.name}</span>
+                        </div>
                     )}
-                    <span className="text-base font-semibold leading-6 text-gray-900">Nombre:</span>
-                    <span className="mb-2">{user.firstname} {user.first_lastname}</span>
-                    <span className="text-base font-semibold leading-6 text-gray-900">Correo:</span>
-                    <span className="mb-2">{user.email}</span>
+                    <div className="mb-6">
+                        <span className="text-lg font-semibold text-gray-700">Nombre:</span>
+                        <span className="text-base text-gray-900 ml-2">{user.firstname} {user.first_lastname}</span>
+                    </div>
+                    <div className="mb-6">
+                        <span className="text-lg font-semibold text-gray-700">Correo:</span>
+                        <span className="text-base text-gray-900 ml-2">{user.email}</span>
+                    </div>
                 </div>
 
                 <div className="flex flex-col sm:w-1/2 sm:ml-20 items-center">
-                    <UserIcon className="h-16 w-16 rounded-full  text-primary mb-12 mt-3"></UserIcon>
-                    <button className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 bg-primary text-white hover:brightness-90 mb-2 sm:mb-0"
-                    onClick={cambiarContraseñaClick}
+                    <UserIcon className="h-20 w-20 rounded-full text-primary mb-8 mt-3"></UserIcon> {/* Aumenta el tamaño de la imagen */}
+                    <button className="w-full rounded-md px-4 py-2 text-base font-semibold bg-primary text-white hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50 mb-4 transition duration-300 ease-in-out transform hover:scale-105"
+                        onClick={cambiarContraseñaClick}
                     >
                         Cambiar Contraseña
                     </button>
-                    <button className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 bg-primary text-white hover:brightness-90 mb-2 sm:mb-0 mt-2"
+                    <button className="w-full rounded-md px-4 py-2 text-base font-semibold bg-primary text-white hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50 transition duration-300 ease-in-out transform hover:scale-105"
                     >
                         Editar Perfil
                     </button>
@@ -48,4 +55,5 @@ export default function Perfil() {
             </div>
         </div>
     );
+
 }
